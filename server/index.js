ea31a503f631fdf4b90d7c94af537f1d78bd9ca0
tjlemperle <tjlemperle@gmail.com>
2020-03-02 14:06:52 -0700

    first node

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const getProducts = require('./getProducts')
+const getProduct = require('./getProduct')
+
+const app = express()
+
+const port = 3001
+
+app.get('/api/products', getProducts)
+
+app.get('/api/product/:id', getProduct)
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`)
+})
+
+
